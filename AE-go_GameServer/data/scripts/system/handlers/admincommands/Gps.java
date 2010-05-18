93d40096b0bcf3c0df0b09ab3a4443d15454025d
Xitanium <xitanium@zetatron.net>
2010-05-18 15:12:05 +0200

    Added //gps admincommand

---
+++ AE-go_GameServer/data/scripts/system/handlers/admincommands/Gps.java
@@ -0,0 +1,33 @@
+package admincommands;
+
+import com.aionemu.gameserver.configs.administration.AdminConfig;
+import com.aionemu.gameserver.model.gameobjects.Gatherable;
+import com.aionemu.gameserver.model.gameobjects.Npc;
+import com.aionemu.gameserver.model.gameobjects.VisibleObject;
+import com.aionemu.gameserver.model.gameobjects.player.Player;
+import com.aionemu.gameserver.utils.PacketSendUtility;
+import com.aionemu.gameserver.utils.chathandlers.AdminCommand;
+
+/**
+ * @author Nemiroff
+ *         Date: 28.12.2009
+ */
+public class Gps extends AdminCommand
+{
+
+    public Gps()
+	{
+        super("info");
+    }
+
+    @Override
+    public void executeCommand(Player admin, String[] params)
+	{
+        PacketSendUtility.sendMessage(admin, "== Your current coordinates ==");
+        PacketSendUtility.sendMessage(admin, "Map = " + admin.getWorldId());
+        PacketSendUtility.sendMessage(admin, "X = " + admin.getX());
+        PacketSendUtility.sendMessage(admin, "Y = " + admin.getY());
+        PacketSendUtility.sendMessage(admin, "Z = " + admin.getZ());
+        PacketSendUtility.sendMessage(admin, "H = " + admin.getHeading());
+    }
+}
