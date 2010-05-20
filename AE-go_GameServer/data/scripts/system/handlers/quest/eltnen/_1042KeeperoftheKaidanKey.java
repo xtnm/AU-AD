7432c269ba2dee76bdc2defa9573a09255df1ecc
Xitanium <xitanium@zetatron.net>
2010-05-20 15:50:36 +0200

    dqzd

--- AE-go_GameServer/data/scripts/system/handlers/quest/eltnen/_1042KeeperoftheKaidanKey.java
+++ AE-go_GameServer/data/scripts/system/handlers/quest/eltnen/_1042KeeperoftheKaidanKey.java
@@ -16,27 +16,34 @@
  */
 package quest.eltnen;
 
+import java.util.Collections;
+
 import com.aionemu.gameserver.model.gameobjects.Item;
+import com.aionemu.gameserver.model.templates.quest.QuestItems;
 import com.aionemu.gameserver.model.gameobjects.Npc;
+import com.aionemu.gameserver.network.aion.serverpackets.SM_EMOTION;
+import com.aionemu.gameserver.network.aion.serverpackets.SM_USE_OBJECT;
 import com.aionemu.gameserver.model.gameobjects.player.Player;
 import com.aionemu.gameserver.network.aion.serverpackets.SM_DIALOG_WINDOW;
-import com.aionemu.gameserver.network.aion.serverpackets.SM_ITEM_USAGE_ANIMATION;
-import com.aionemu.gameserver.network.aion.serverpackets.SM_PLAY_MOVIE;
 import com.aionemu.gameserver.questEngine.handlers.QuestHandler;
 import com.aionemu.gameserver.questEngine.model.QuestEnv;
+import com.aionemu.gameserver.network.aion.serverpackets.SM_ITEM_USAGE_ANIMATION;
 import com.aionemu.gameserver.questEngine.model.QuestState;
+import com.aionemu.gameserver.utils.ThreadPoolManager;
 import com.aionemu.gameserver.questEngine.model.QuestStatus;
 import com.aionemu.gameserver.utils.PacketSendUtility;
+import com.google.inject.Inject;
 
 /**
- * @author Rhys2002 corrected by xitanium
+ * @author Nephis
  * 
  */
-public class _1042KeeperoftheKaidanKey extends QuestHandler
+public class _1042KeeperOfTheKaidanKey extends QuestHandler
 {
+
 	private final static int	questId	= 1042;
-	
-	public _1042KeeperoftheKaidanKey()
+
+	public _1042KeeperOfTheKaidanKey()
 	{
 		super(questId);
 	}
@@ -44,97 +51,23 @@ public class _1042KeeperoftheKaidanKey extends QuestHandler
 	@Override
 	public void register()
 	{
+		qe.setQuestItemIds(182201026).add(questId); //Kalupa's Order
+		qe.setNpcQuestData(203989).addOnTalkEvent(questId); //Tumblusen
+		qe.setNpcQuestData(203901).addOnTalkEvent(questId); //Telemachus
 		qe.addQuestLvlUp(questId);
-		qe.setNpcQuestData(203989).addOnTalkEvent(questId);
-		qe.setNpcQuestData(203901).addOnTalkEvent(questId);
-		qe.setQuestItemIds(182201026).add(questId);		
 	}
-
+	
 	@Override
 	public boolean onLvlUpEvent(QuestEnv env)
 	{
-		final Player player = env.getPlayer();
-		final QuestState qs = player.getQuestStateList().getQuestState(questId);
-		if(qs == null || qs.getStatus() != QuestStatus.LOCKED || player.getCommonData().getLevel() < 35)
-			return false;
-
-		QuestState qs2 = player.getQuestStateList().getQuestState(1040);
-		if(qs2 == null || qs2.getStatus() != QuestStatus.COMPLETE)
+		Player player = env.getPlayer();
+		QuestState qs = player.getQuestStateList().getQuestState(questId);
+		if(qs == null || player.getCommonData().getLevel() < 35 || qs.getStatus() != QuestStatus.LOCKED)
 			return false;
 		qs.setStatus(QuestStatus.START);
 		updateQuestStatus(player, qs);
 		return true;
-	}
-
-	@Override
-	public boolean onDialogEvent(QuestEnv env)
-	{
-		final Player player = env.getPlayer();
-		final QuestState qs = player.getQuestStateList().getQuestState(questId);
-		if(qs == null)
-			return false;
-
-		int var = qs.getQuestVarById(0);
-		int targetId = 0;
-		if(env.getVisibleObject() instanceof Npc)
-			targetId = ((Npc) env.getVisibleObject()).getNpcId();
-
-		if(qs.getStatus() == QuestStatus.REWARD)
-		{
-			if(targetId == 203901)
-			{
-				if(env.getDialogId() == -1)
-					return sendQuestDialog(player, env.getVisibleObject().getObjectId(), 10002);
-				else if(env.getDialogId() == 1009)
-					return sendQuestDialog(player, env.getVisibleObject().getObjectId(), 5);
-				else return defaultQuestEndDialog(env);
-			}
-			return false;
-		}
-		else if(qs.getStatus() != QuestStatus.START)
-		{
-			return false;
-		}
-		if(targetId == 203989)
-		{
-			switch(env.getDialogId())
-			{
-				case 25:
-					if(var == 0)
-						return sendQuestDialog(player, env.getVisibleObject().getObjectId(), 1011);
-				case 1012:
-					PacketSendUtility.sendPacket(player, new SM_PLAY_MOVIE(0, 185));
-						break;
-				case 10000:
-					if(var == 0)
-					{
-						qs.setQuestVarById(0, var + 1);
-						updateQuestStatus(player, qs);
-						PacketSendUtility.sendPacket(player, new SM_DIALOG_WINDOW(env.getVisibleObject().getObjectId(), 10));
-						return true;
-					}
-			}
-		}
-		else if(targetId == 203901)
-		{
-			switch(env.getDialogId())
-			{
-				case 25:
-					if(var == 2)
-						return sendQuestDialog(player, env.getVisibleObject().getObjectId(), 1352);
-				case 33:
-					if(questService.collectItemCheck(env, true))				
-					{
-						qs.setStatus(QuestStatus.REWARD);					
-						updateQuestStatus(player, qs);					
-						return sendQuestDialog(player, env.getVisibleObject().getObjectId(), 5);
-					}
-					else
-						return sendQuestDialog(player, env.getVisibleObject().getObjectId(), 1438);				
-			}
-		}		
-		return false;
-	}
+	}	
 	
 	@Override
 	public boolean onItemUseEvent(QuestEnv env, Item item)
@@ -142,14 +75,88 @@ public class _1042KeeperoftheKaidanKey extends QuestHandler
 		final Player player = env.getPlayer();
 		final int id = item.getItemTemplate().getTemplateId();
 		final int itemObjId = item.getObjectId();
-
-		final QuestState qs = player.getQuestStateList().getQuestState(questId);
-		if(qs.getQuestVarById(0) !=1)
+		final QuestState qs = player.getQuestStateList().getQuestState(questId);	
+		if(id != 182201026) //TODO: Fix dialog
 			return false;
-		PacketSendUtility.broadcastPacket(player, new SM_ITEM_USAGE_ANIMATION(player.getObjectId(), itemObjId, id, 20, 1, 0), true);
-		player.getInventory().removeFromBagByItemId(182201026, 1);
-		qs.setQuestVarById(0, 2);
-		updateQuestStatus(player, qs);		
+		PacketSendUtility.broadcastPacket(player, new SM_ITEM_USAGE_ANIMATION(player.getObjectId(), itemObjId, id,
+			3000, 0, 0), true);		
+		ThreadPoolManager.getInstance().schedule(new Runnable(){			
+			@Override			
+			public void run()
+			{
+				player.getInventory().removeFromBagByItemId(182201026, 1);
+				qs.setQuestVar(2);
+				updateQuestStatus(player, qs);			
+				PacketSendUtility.broadcastPacket(player, new SM_ITEM_USAGE_ANIMATION(player.getObjectId(), itemObjId,
+					id, 0, 1, 0), true);
+			}
+		}, 3000);
 		return true;
-	}		
+	}
+	
+	@Override
+	public boolean onDialogEvent(QuestEnv env)
+	{
+		final Player player = env.getPlayer();
+		int targetId = 0;
+		if(env.getVisibleObject() instanceof Npc)
+			targetId = ((Npc) env.getVisibleObject()).getNpcId();
+		QuestState qs = player.getQuestStateList().getQuestState(questId);
+		if(qs == null)
+		return false;
+		if(targetId == 203989) //Tumblusen
+		{
+			if(qs.getStatus() == QuestStatus.START && qs.getQuestVarById(0) == 0)
+			{
+				if(env.getDialogId() == 25)
+					return sendQuestDialog(player, env.getVisibleObject().getObjectId(), 1011);
+				else if(env.getDialogId() == 10000)
+					{
+					qs.setQuestVar(1);
+                    updateQuestStatus(player, qs);
+					PacketSendUtility
+						.sendPacket(player, new SM_DIALOG_WINDOW(env.getVisibleObject().getObjectId(), 10));
+					return true;
+					}
+				else
+					return defaultQuestStartDialog(env);
+			}
+			
+			else if(qs.getStatus() == QuestStatus.REWARD)
+			{
+				return defaultQuestEndDialog(env);
+			}
+		}
+		else if(targetId == 203901) //Telemachus
+		{
+			if(qs.getStatus() == QuestStatus.START && qs.getQuestVarById(0) == 2)
+			{
+				if(env.getDialogId() == 25)
+					return sendQuestDialog(player, env.getVisibleObject().getObjectId(), 1352);
+				else if(env.getDialogId() == 33)
+				{
+					if(player.getInventory().getItemCountByItemId(182201018) > 0)
+					{
+						player.getInventory().removeFromBagByItemId(182201018, 1);
+						qs.setStatus(QuestStatus.REWARD);
+						updateQuestStatus(player, qs);
+						PacketSendUtility
+						.sendPacket(player, new SM_DIALOG_WINDOW(env.getVisibleObject().getObjectId(), 10));
+						return true;
+					}
+					else
+						return sendQuestDialog(player, env.getVisibleObject().getObjectId(), 1438);
+				}
+				else
+					return defaultQuestStartDialog(env);
+			}
+			
+			else if(qs.getStatus() == QuestStatus.REWARD)
+			{
+				return defaultQuestEndDialog(env);
+			}
+		}
+		return false;
+
+	}
 }
